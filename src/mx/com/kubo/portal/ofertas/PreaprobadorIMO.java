4a8bd902dc2a106198a4fc2f6934022436d2a210
damian <damian.tapia.nava@gmail.com>
2016-12-14 12:37:41 -0600

    [kubo.portal-14DIC16]

--- src/mx/com/kubo/portal/ofertas/PreaprobadorIMO.java
+++
@@ -1,8 +0,0 @@
-package mx.com.kubo.portal.ofertas;
-
-import mx.com.kubo.mesa.solicitud.adicional.ReasignadorIMO;
-
-public interface PreaprobadorIMO 
-{	
-	void setReasignador(ReasignadorIMO reasignador);
-}
